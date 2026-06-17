9238116426c61d04f0733e1c82fe1a83fc832537
ALEXANDRE FERREIRA <56090159+4lxcf@users.noreply.github.com>
2026-06-17 20:09:57 -0300

    Add program to find the largest integer from input

---
+++ 1_semestre/LinguagemDeProgramacao/ListaAVetoresMatrizesFuncoes/questao07/q07.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int vec[15], maior = 0;
+
+    for (int i = 0; i < 15; i++)
+    {
+        printf("Digite um numero inteiro: \n");
+        scanf(" %d", &vec[i]);
+        
+        
+		if(vec[i] >= maior){
+        	maior = vec[i];
+		}
+		
+		printf("O MAIOR ATE AGORA = %d\n", maior);
+    }
+    
+    printf("O maior valor digitado foi: %d", maior);
+
+    return 0;
+}
