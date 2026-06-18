1348eadb291a38301f39ae21b909002cfe70c7af
ALEXANDRE FERREIRA <56090159+4lxcf@users.noreply.github.com>
2026-06-18 20:54:52 -0300

    Add matrix input and row sum calculation in q12.c

---
+++ 1_semestre/LinguagemDeProgramacao/ListaAVetoresMatrizesFuncoes/questao12/q12.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	float matrix[4][4], soma;
+	
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			printf("Digite um valor para a posicao [%d][%d] da matriz.\n", i, j);
+			scanf("%f", &matrix[i][j]);
+		}	
+	}
+	
+	for(int i=0; i<4; i++){
+		soma = 0;
+		for(int j=0; j<4; j++){
+			printf("| %.2f ", matrix[i][j]);
+			soma += matrix[i][j];
+		}
+		printf("| Soma da Linha %d: %.2f\n", i, soma
+		);
+	}
+	
+	return 0;
+}
