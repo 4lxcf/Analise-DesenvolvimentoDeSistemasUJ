afbce7347c516d4ead353f34d308c9a60300f839
4lxcf <alexandrecferreira11@gmail.com>
2026-07-05 22:58:39 -0300

    questao12 feita!

--- 1_semestre/LinguagemDeProgramacao/ListaAvaliativaAV1/questao12/q12.c
+++ 1_semestre/LinguagemDeProgramacao/ListaAvaliativaAV1/questao12/q12.c
@@ -3,23 +3,45 @@
 
 void ordenar(int v[], int n){
     int next, aux;
+    int ordered = 0;
 
-    for (int i = 0; i < n - 1; i++){
-        next = v[i+1];
-        if (v[i] > next){
-            aux = v[i];
-            v[i] = next;
-            v[i+1] = aux;
+    while (ordered < (n - 1)){
+        ordered = 0;
+        //Ordenação do vetor
+        for (int i = 0; i < n - 1; i++){
+            next = v[i+1];
+            if (v[i] > next){
+                aux = v[i];
+                v[i] = next;
+                v[i+1] = aux;
+            } else {
+                ordered++;
+            }
         }
     }
 }
 
 int buscaBinaria(int v[], int n, int chave){
-    return 0;
+    int inicio = 0, fim = n - 1;
+
+    while (inicio <= fim){
+        int meio = inicio + (fim - inicio) / 2;
+
+        if (v[meio] == chave){
+            return meio;
+        } else if (v[meio] > chave){
+            fim = meio - 1;
+        } else {
+            inicio = meio + 1;
+        }
+        
+    }    
+    
+    return -1;
 }
 
 int main(){
-    int qtd;
+    int qtd, chave, pos;
 
     printf("Digite quantos valores serao utilizados no vetor (1~50).\n");
     scanf("%d", &qtd);
@@ -56,7 +78,7 @@ int main(){
     ordenar(vet, qtd);
 
     //Mostrar o vetor ordenado
-    printf("Vetor Ordenado = [ ");
+    printf("Bubble Sort = [ ");
     for (int i = 0; i < qtd; i++){
         if (i == (qtd - 1)){
             printf("%d", vet[i]);
@@ -66,5 +88,16 @@ int main(){
     }
     printf(" ]\n");
 
+    printf("Agora digite o valor que deseja buscar:\n");
+    scanf("%d", &chave);
+
+    pos = buscaBinaria(vet, qtd, chave);
+
+    if (pos == -1){
+        printf("Numero nao encontrado.");
+        return -1;
+    }
+
+    printf("O numero >%d< foi encontrado na posicao [%d] do vetor.", chave, pos);
     return 0;
 }
